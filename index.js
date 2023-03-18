d2be03d6dda5c6bfedabd58913397a464d245eaf
Thu Thao <vuthithuthao132@gmail.com>
2023-03-18 10:23:16 +0700

    second commit

---
+++ index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
+
+const url = "mongodb://127.0.0.1:27017/Tour";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(url);
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
